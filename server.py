093544419dbab5686b9d672d8b1dacf648d3ba22
Willem Dauwen <willem.dauwen@gmail.com>
2016-04-05 19:34:11 +0200

    commit controls

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.websocket
+import json
+
+from robotics import robot3 as robot
  
 from tornado.options import define, options
 define("port", default=8080, help="run on the given port", type=int)
@@ -13,17 +16,32 @@ class IndexHandler(tornado.web.RequestHandler):
  
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
-        print 'new connection'
+        print('new connection')
         self.write_message("connected")
  
     def on_message(self, message):
         print 'message received %s' % message
         self.write_message('message received %s' % message)
+        mapping = {
+            # message from websocket to function called
+            'forward': robot.forward,
+            'reverse': robot.reverse,
+        }
+        message_dict = json.loads(message)
+        for command, value in message_dict.items():
+            if command in mapping:
+                print("Launching command: '{}'".format(command))
+                action = mapping[command]
+                action(value)
+            else:
+                print("Unknown command: '{}'".format(command))
  
     def on_close(self):
         print 'connection closed'
  
 if __name__ == "__main__":
+    robot.init()
+
     tornado.options.parse_command_line()
     app = tornado.web.Application(
         handlers=[
